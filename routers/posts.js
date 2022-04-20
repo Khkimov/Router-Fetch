c4e620e6deeca25f5be0241f3d85a186a8aad905
Ilyas <khakim198765@gmail.com>
2022-04-20 19:53:16 +0500

    Express and router

---
+++ routers/posts.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { Post, User } = require('../db/models');
+
+router.get('/all', async (req, res) => {
+  try {
+    const posts = await Post.findAll({raw: true});
+    res.render('posts', { Posts: posts })
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+})
+
+// /posts/
+router.get('/', async (req, res) => {
+  const {name} = req.query;
+  try {
+    const userPost = (await User.findOne({where: {name}, include: [Post]})).dataValues;
+    userPost.Posts = userPost.Posts.map(el => el.dataValues);
+    res.render('posts', { Posts: userPost.Posts, name: userPost.name});
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+})
+
+
+
+
+module.exports = router;
---
+++ routers/user.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { User } = require('../db/models');
+const router = express.Router();
+
+
+// /user/
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll({raw: true});
+    res.render('users', { users });
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+})
+
+module.exports = router;
